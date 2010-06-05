2c49af51cb74dac70a2f3f242d1d63408a22749f
William Scales <william.scales@gmail.com>
2010-06-05 16:14:20 -0600

    basic skeleton for regina

--- lib/regina.rb
+++ lib/regina.rb
@@ -1,3 +1,35 @@
-class Regina
-  VERSION = '1.0.0'
+module Regina
+  VERSION = 0.0.1
+
+  class Parser
+    
+    def initialize *a, &b
+      cloaker(&b).bind(self).call(*a)
+    end
+
+    def foo
+      puts "whee"
+    end
+
+    # thanks to _why
+    def cloaker &b
+      (class << self; self; end).class_eval do
+        define_method :cloaker_, &b
+        meth = instance_method :cloaker_
+        remove_method :cloaker_
+        meth
+      end
+    end
+
+  end
+
+  def new *a, &b
+    Parser.new *a, &b if b
+  end
+  module_function :new
+
+end
+
+Regina.new do
+  foo
 end
